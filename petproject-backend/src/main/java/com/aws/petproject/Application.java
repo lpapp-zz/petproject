f4d014c42ccc843805a6fd8cadd2450170e0d653
Papp, Levente <levente.papp@ge.com>
2016-10-03 15:56:53 +0200

    Exclude services from autoconfig

--- petproject-backend/src/main/java/com/aws/petproject/Application.java
+++ petproject-backend/src/main/java/com/aws/petproject/Application.java
@@ -1,7 +1,14 @@
 package com.aws.petproject;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.ProxyCachingConfiguration;
+import org.springframework.cloud.aws.autoconfigure.cache.ElastiCacheAutoConfiguration;
+import org.springframework.cloud.aws.autoconfigure.context.ContextStackAutoConfiguration;
+import org.springframework.cloud.aws.autoconfigure.mail.MailSenderAutoConfiguration;
+import org.springframework.cloud.aws.core.env.stack.StackResourceRegistry;
+import org.springframework.cloud.aws.core.env.stack.config.StackResourceRegistryFactoryBean;
 import org.springframework.context.annotation.ComponentScan;
 
 /**
@@ -9,6 +16,14 @@ import org.springframework.context.annotation.ComponentScan;
  */
 @SpringBootApplication
 @ComponentScan(basePackages = {"com.aws.petproject"})
+@EnableAutoConfiguration(exclude = {
+                                     ElastiCacheAutoConfiguration.class,
+                                     MailSenderAutoConfiguration.class,
+                                     StackResourceRegistryFactoryBean.class,
+                                     ContextStackAutoConfiguration.class,
+                                     StackResourceRegistry.class,
+                                     ProxyCachingConfiguration.class
+})
 public class Application {
 
     public static void main( String[] args ) {
